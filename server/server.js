1d3ec5935cabfd2c40aedfdfde0f4cd397598ba6
Nicholas Basinger <nbasinger@collegeboard.org>
2019-01-08 15:18:08 -0500

    Added PATCH

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
-let express = require('express');
-let bodyParser = require('body-parser');
-let { ObjectID } = require('mongodb');
+const _ = require('lodash')
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 let { mongoose } = require('./db/mongoose');
 let { Todo } = require('./models/todo');
@@ -68,6 +70,30 @@ app.delete('/todos/:id', (req,res) => {
   })
 });
 
+app.patch('/todos/:id', (req,res) => {
+  let id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)){
+    res.status(404).send('Whoops! ID not valid.')
+  };
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  };
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+    res.send({todo})
+  }).catch((e) => {
+    res.status(400).send(e);
+  });
+});
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`)
