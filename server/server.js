39feaef3a78770b8c526eceb87e2b1d2489d4067
Nicholas Basinger <nbasinger@collegeboard.org>
2019-01-10 09:52:05 -0500

    Setup seperate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+require('./config/config')
+
 const _ = require('lodash')
 
 const express = require('express');
@@ -9,7 +11,7 @@ let { Todo } = require('./models/todo');
 let { User } = require('./models/user');
 
 let app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
